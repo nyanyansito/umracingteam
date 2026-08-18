b2182a285caf7e8260e0d70edea56475505891c3
nyanyansito <celestetrinity05@gmail.com>
2026-08-18 23:28:55 +0800

    first commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('beautiful pig')
+    print_hi('beautiful piggy')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
